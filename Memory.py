7f036739b8e53b788b81306db98a9b64ada72258
Alex Jiakai Xu <ax2155@columbia.edu>
2025-01-31 10:56:22 -0500

    Implement Memory

--- Memory.py
+++ Memory.py
@@ -4,5 +4,20 @@ class Memory:
     """
     Simulated shared resource that can hold up to `capacity` tokens total.
     """
+
+    def __init__(self, env, capacity):
+        self.env = env
+        self.capacity = capacity
+        self.container = simpy.Container(env, init=capacity, capacity=capacity)
+
+    def request(self, amount):
+        return self.container.get(amount)
+
+    def release(self, amount):
+        return self.container.put(amount)
+
+    def available_tokens(self):
+        return self.container.level
+
     def __str__(self):
-        return "Memory"
+        return f"Memory: {self.available_tokens()}/{self.capacity} tokens available"
