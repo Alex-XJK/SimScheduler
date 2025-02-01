c97ecb49015c4417fb9761d0332dd6fc64b0d656
Alex Jiakai Xu <ax2155@columbia.edu>
2025-02-01 15:17:30 -0500

    Explore parameters

--- main.py
+++ main.py
@@ -14,27 +14,27 @@ def main():
     logging.basicConfig(level=logging.INFO, format='%(message)s')
 
     # 2. Define Memory Resource
-    memory = Memory(env, capacity=100)
+    memory = Memory(env, capacity=60000)
 
     # 3. Define Scheduler
     # scheduler = FCFSScheduler(env, memory=memory)
     # scheduler = RRScheduler(env, memory=memory, time_slice=1)
-    scheduler = RRSmartScheduler(env, memory=memory, time_slice=1, threshold=0.8)
+    scheduler = RRSmartScheduler(env, memory=memory, time_slice=100, threshold=0.7)
 
     # 4. Define Generator
     def random_M():
-        return random.randint(10, 15)
-    generator = Generator(env, scheduler=scheduler, speed=2, total=10, init_size=50, final_fn=random_M)
+        return random.randint(256, 2048)
+    generator = Generator(env, scheduler=scheduler, speed=2, total=1000, init_size=512, final_fn=random_M)
 
     # 4. Create the System
     system = System(env, memory=memory, scheduler=scheduler, generator=generator)
-    print(system)
 
     # 5. Run the simulation
-    env.process(system.run_simulation(max_time=100))
+    env.process(system.run_simulation(max_time=100000))
     env.run()
 
     # 6. Print results
+    print(system)
     system.report_stats()
 
 
