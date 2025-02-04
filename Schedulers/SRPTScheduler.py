0a55d7ff15c6eef88f5709481e4c5e85149e6016
Alex Jiakai Xu <ax2155@columbia.edu>
2025-02-04 14:33:45 -0500

    Implement SRPT Scheduler

---
+++ Schedulers/SRPTScheduler.py
@@ -0,0 +1,16 @@
+import simpy
+from Scheduler import Scheduler
+from Job import Job
+
+
+class SRPTScheduler(Scheduler):
+    """
+    Shortest Remaining Processing Time (SRPT) Scheduler.
+    """
+
+    def __init__(self, env, memory):
+        super().__init__(env, memory, "SRPT")
+
+    def pick_next_task(self) -> Job:
+        next_job = min(self.run_queue, key=lambda job: ((job.final_size - job.current_size), job.arrival_time))
+        return next_job
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from Schedulers.FCFSScheduler import FCFSScheduler
 from Schedulers.RRScheduler import RRScheduler
 from Schedulers.RRRejectScheduler import RRRejectScheduler
 from Schedulers.RRSwapScheduler import RRSwapScheduler
+from Schedulers.SRPTScheduler import SRPTScheduler
 
 def main():
     # 1. Create SimPy Environment
@@ -21,7 +22,8 @@ def main():
     # scheduler = FCFSScheduler(env, memory=memory)
     # scheduler = RRScheduler(env, memory=memory, time_slice=1)
     # scheduler = RRRejectScheduler(env, memory=memory, time_slice=100, threshold=0.7)
-    scheduler = RRSwapScheduler(env, memory=memory, time_slice=100, threshold=0.9)
+    # scheduler = RRSwapScheduler(env, memory=memory, time_slice=100, threshold=0.9)
+    scheduler = SRPTScheduler(env, memory=memory)
 
     # 4. Define Generator
     def random_M():
